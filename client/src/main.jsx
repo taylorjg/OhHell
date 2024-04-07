8fb8364cd50a0d12a5dcbff119fb197057fab678
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2024-04-07 11:11:08 +0100

    Add Material UI

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  createTheme,
+  ThemeProvider,
+  CssBaseline,
+  responsiveFontSizes,
+} from "@mui/material";
+import { Global } from "@emotion/react";
 
 import { App } from "./App.jsx";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import { GlobalStyles } from "./Global.styles";
+
+const darkTheme = responsiveFontSizes(
+  createTheme({
+    palette: {
+      mode: "dark",
+    },
+  })
+);
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,8 +39,12 @@ const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <Global styles={GlobalStyles} />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
